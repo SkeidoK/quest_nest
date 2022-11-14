d0686470aaf23e1a1e2f0ddfa11675a6f47bcd94
kylian moreira <kylianmoreira@gmail.com>
2022-11-14 12:24:31 +0100

    init

---
+++ src/users/users.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { User } from './user.entity/user.entity';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private service: UsersService) {}
+
+  @Get(':id')
+  get(@Param() params) {
+    return this.service.getUser(params.id);
+  }
+
+  @Post()
+  create(@Body() user: User) {
+    return this.service.saveUser(user);
+  }
+
+  @Put()
+  update(@Body() user: User) {
+    return this.service.saveUser(user);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param() params) {
+    this.service.deleteUser(params.id);
+    return;
+  }
+}
